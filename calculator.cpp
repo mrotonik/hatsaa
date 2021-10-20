b0ba8e20b5867137cf632c94fe9a3d8ab6a8b411
mrotonik <antocha20011@ukr.net>
2021-10-20 23:20:59 +0300

    add patches

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,11 @@
 
 int Calculator::Add (double a, double b)
 {
+<<<<<<< HEAD
 	return a + b + 0.5;
+=======
+	return a + b;
+>>>>>>> cbcb06d (formatting: use tabs instead of spaces)
 }
 
 int Calculator::Sub (double a, double b)
